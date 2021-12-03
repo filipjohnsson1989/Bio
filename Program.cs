13ab6cfeedf6dc45484d385c354d3f077ccd9a77
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-03 10:38:17 +0100

    Add a main menu with the choices

--- Program.cs
+++ Program.cs
@@ -1,11 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bio
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            // Huvudmeny
+            do
+            {
+                Console.WriteLine("Huvudmeny");
+                Console.WriteLine("0: Stäng ner programmet!");
+                Console.WriteLine("1: Ungdom eller pensionär");
+                Console.WriteLine("2: Upprepa tio gånger!");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    case "1":
+                        throw new NotImplementedException();
+                        break;
+                    case "2":
+                        throw new NotImplementedException();
+                        break;
+                    default:
+                        Console.WriteLine("Det är felaktig input");
+                        break;
+                }
+            } while (true);
+
         }
     }
 }
