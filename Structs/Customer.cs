f7228545546f16394a7bd0a6c1ac05f3e9e3368c
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-03 12:12:58 +0100

    Add customer struct

---
+++ Structs/Customer.cs
@@ -0,0 +1,16 @@
+﻿using Bio.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bio.Structs
+{
+    struct Customer
+    {
+        string Name;
+        CustomerType Type;
+        uint Price;
+    }
+}
