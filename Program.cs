e4a38818e2b26920a2dc208c829fcaf9f56a2c3d
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-03 15:39:11 +0100

    name correction, use seed at the beginning of the program, fix wrong prices for different custom types in the definition

--- Program.cs
+++ Program.cs
@@ -7,9 +7,10 @@ namespace Bio
 {
     class Program
     {
-        private static CustomerCondition[] CustomerConditions = new CustomerCondition[5];
+        private static CustomerCondition[] customerConditions = new CustomerCondition[5];
         static void Main(string[] args)
         {
+            SeedData();
             // Huvudmeny
             do
             {
@@ -40,20 +41,19 @@ namespace Bio
         private static void SeedData()
         {
             AddCustomerCondition(customerType: CustomerType.Child, minAge: 0, price: 0);
-            AddCustomerCondition(customerType: CustomerType.Young, minAge: 5, price: 0);
-            AddCustomerCondition(customerType: CustomerType.Adult, minAge: 20, price: 0);
-            AddCustomerCondition(customerType: CustomerType.Pensioner, minAge: 65, price: 0);
+            AddCustomerCondition(customerType: CustomerType.Young, minAge: 5, price: 80);
+            AddCustomerCondition(customerType: CustomerType.Adult, minAge: 20, price: 120);
+            AddCustomerCondition(customerType: CustomerType.Pensioner, minAge: 65, price: 90);
             AddCustomerCondition(customerType: CustomerType.Centenarian, minAge: 100, price: 0);
         }
 
         private static void AddCustomerCondition(CustomerType customerType, uint minAge, uint price)
         {
-            var customerCondition = CustomerConditions[(int)customerType];
+            var customerCondition = customerConditions[(int)customerType];
             customerCondition.Type = customerType;
             customerCondition.MinAge = minAge;
             customerCondition.Price = price;
         }
-
 
 
     }
