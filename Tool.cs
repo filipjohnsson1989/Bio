1f45ee6d4175dce7dd8a2dc3da0ab949909f1f68
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-06 16:55:27 +0100

    The Tool class is commented.

--- Tool.cs
+++ Tool.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace Bio
 {
+    //En generisk klass för att få en giltig input för olika typer av input från användaren.
     static class Tool<T>
     {
+        //infoPrompt används för den första prompten. errorPrompt används för ogiltig inmatning.
         public static T AskForAnInput(string infoPrompt, string errorPrompt)
         {
             T result;
 
-
+            //Det är specialt för string type annars fick vi runtime del.
             if (typeof(T) == typeof(string)) result = (T)Activator.CreateInstance(typeof(T), string.Empty.ToCharArray());
             else result = Activator.CreateInstance<T>();
 
@@ -26,7 +28,9 @@ namespace Bio
 
                 try
                 {
+                    //Om appen inte kan ändra den önskade typen ger den ett exception. Så vi måste hantera det med try-catch.
                     result = (T)Convert.ChangeType(answer, typeof(T));
+                    //Om användaren inte anger en ingång gör vi ett exception för att göra samma sak eftersom det är en ogiltig inmatning.
                     if (string.IsNullOrWhiteSpace(answer)) throw new Exception();
 
                     success = true;
