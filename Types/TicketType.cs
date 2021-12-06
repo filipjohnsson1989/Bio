ea1a957945594bfac5253b93beac98246d2a0bd2
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-06 11:06:59 +0100

    Add ticket type

---
+++ Types/TicketType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bio.Types
+{
+    enum TicketType
+    {
+        Free = 0,
+        Young = 80,
+        Pensioner = 90,
+        Adult = 120,
+    }
+}
